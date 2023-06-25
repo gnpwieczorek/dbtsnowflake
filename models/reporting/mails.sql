3e763686be8e4b766553bde8747e9cd99da9cd27
pwieczo@gmail.com <pwieczo@gmail.com>
2023-06-25 11:13:12 +0000

    test adding

--- models/reporting/mails.sql
+++ models/reporting/mails.sql
@@ -1,31 +1,23 @@
-{{
-    config(
-        materialized='table'
-        , tags=['hestia','mails']
+{{ config(materialized="table", tags=["hestia", "mails"]) }}
+
+with
+    final_mails as (
+        select
+            nation as notion,
+            company as company,
+            split_part(adress_email, '@', 2) as mail_domain
+        from {{ ref("stg_clients_mails") }}
+    ),
+    domain_aggregation as (
+        select
+            count(*) as domain_usage,
+            mail_domain,
+            regexp_replace(notion, '[^a-zA-Z]', '') as nation,
+            regexp_replace(company, '[^a-zA-Z]', '') as company
+        from final_mails
+        group by 2, 3, 4
+
     )
-}}
 
-with final_mails as
-(
-    select  
-        NATION as notion
-        ,COMPANY as COMPANY
-        ,split_part(ADRESS_EMAIL,'@',2) as mail_domain
-    from
-        {{ ref("stg_clients_mails") }}
-)
-, domain_aggregation as (
-    select 
-        count(*) as doamin_usage
-        ,mail_domain
-        ,regexp_replace(notion,'[^a-zA-Z]','') as notion
-        ,regexp_replace(COMPANY,'[^a-zA-Z]','') as COMPANY
-    from    
-        final_mails
-    group by
-        2,3,4
-
-)
-
-select * from domain_aggregation
-
+select *
+from domain_aggregation
--- models/staging/stg_clients.sql
+++ models/staging/stg_clients.sql
@@ -1,39 +1,34 @@
-{{
-    config(
-        materialized="incremental"
-        , tags=['hestia','clients']
+{{ config(materialized="incremental", tags=["hestia", "clients"]) }}
+
+
+with
+    stg_client as (
+
+        select
+            adress_email as adress_email,
+            company as company,
+            credit_card as credit_card,
+            first_name as first_name,
+            id as id,
+            last_name as last_name,
+            nation as nation,
+            passport_number as passport_number,
+            pesel as pesel,
+            phone_number as phone_number,
+            op as op,
+            transaction as transaction,
+            ts_ms as ts_ms,
+            current_timestamp as added_at
+
+        from {{ ref("raw_client") }}
+
+        {% if is_incremental() %}
+
+            where added_at > (select max(added_at) from {{ this }})
+
+        {% endif %}
+        qualify row_number() over (partition by id order by ts_ms desc) = 1
     )
-}}
 
-
-with stg_client as (
-
-    select
-          ADRESS_EMAIL              as ADRESS_EMAIL
-        , COMPANY           as COMPANY
-        , CREDIT_CARD                 as CREDIT_CARD
-        , FIRST_NAME           as FIRST_NAME
-        , ID            as ID
-        , LAST_NAME          as LAST_NAME
-        , NATION                  as NATION
-        , PASSPORT_NUMBER as PASSPORT_NUMBER
-        , PESEL as PESEL
-        , PHONE_NUMBER as PHONE_NUMBER
-        , OP as OP
-        , TRANSACTION as TRANSACTION
-        , TS_MS as TS_MS
-        , current_timestamp    as added_at
-
-
-
-    from {{ ref("raw_client") }}
-
-    {% if is_incremental() %}
-
-        where added_at > (select max(added_at) from {{ this }})
-
-    {% endif %}
-    qualify row_number() over (partition by id order by ts_ms desc) = 1
-)
-
-select * from stg_client
+select *
+from stg_client
